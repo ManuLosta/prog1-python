3c35b360429eb75198d9f35fa0dd28ad7149f724
ManuLosta <mlostalom@gmail.com>
2022-04-01 20:22:52 -0300

    improved code

--- practice3/ej1.py
+++ practice3/ej1.py
@@ -1,21 +1,26 @@
 import math
 
+# Create a dictionary with the coefficients of the equation
 coefs = {"a": 0.0, "b": 0.0, "x1": 0.0, "x2": 0.0}
 
-# Assign values to coefs
-for i in coefs:
-    coefs[i] = float(input(f"Ingrese el coeficiente {i.upper()}: "))
+# Assign values to coefficients
+for key in coefs:
+    coefs[key] = float(input(f"Ingrese el coeficiente {key.upper()}: "))
 
-# Print coefs values
-for i in coefs:
-    print(f"El coeficiente {i.upper()} de su ecuación de la recta es: {coefs[i]}")
+# Print coefficients values
+for key in coefs:
+    print(f"El coeficiente {key.upper()} de su ecuación de la recta es: {coefs[key]}")
 
+# Print equation
 print(f"\nPara la siguiente ecuación:\nY = {coefs['a']}X + {coefs['b']}")
 
+# Calculate y1 and y2
 y1 = coefs["a"] * coefs["x1"] + coefs["b"]
 y2 = coefs["a"] * coefs["x2"] + coefs["b"]
 
+# Print both points
 print(f"\nDados los siguiente puntos:\n\tP1 ({coefs['x1']}, {y1})\n\tP2 ({coefs['x2']}, {y2})")
 
-distance = math.sqrt((coefs['x1'] - coefs['x2']) ** 2 + (y1 - y2)**2) 
+# Calculate the distance between the points and print it
+distance = math.sqrt((coefs["x1"] - coefs["x2"]) ** 2 + (y1 - y2) ** 2)
 print(f"La distancia entre ellos es: {distance}")
