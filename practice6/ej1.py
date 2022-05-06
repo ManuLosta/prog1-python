527503c9365e9188a69bc1695ff496b80e2cf73f
ManuLosta <mlostalom@gmail.com>
2022-05-06 20:12:34 -0300

    fixed tp6

--- practice6/ej1.py
+++ practice6/ej1.py
@@ -4,11 +4,15 @@ def printList(list):
     for i in range(len(list)):
         print(f"{i}. \t {list[i]}")
 
-def printBackwards(string):
-    result = ""
-    for i in range(1, len(string) + 1):
-        result += string[-i]
-    print(result)
+def printBackwards(list):
+    for i in range(-1, -len(list) - 1, -1):
+        string = ""
+        for j in range(-1, -len(list[i]) - 1, -1):
+            string += list[i][j]
+    
+        print(f"{-i - 1}. \t {string}")
+
 
 printList(sample_list)
-printBackwards('corcho')
+print("\n")
+printBackwards(sample_list)
